5e4a39d4b4f633508a25f40c413c063807267686
yesseri <henrik.zenkert@gmail.com>
2020-12-30 23:51:44 +0100

    added all letters

--- src/morse.rs
+++ src/morse.rs
@@ -37,29 +37,83 @@ pub mod morse {
     impl From<char> for MorseLetter {
         fn from(letter: char) -> Self {
             let morse_letter = match letter {
-                'B' | 'b' => MorseLetter {
-                    morse: vec![Dash, Dot, Dot, Dot],
+                'A' | 'a' => MorseLetter {
+                    morse: vec![Dot, Dash, LetterBreak],
                 },
                 'B' | 'b' => MorseLetter {
-                    morse: vec![Dash, Dot, Dot, Dot],
+                    morse: vec![Dash, Dot, Dot, Dot, LetterBreak],
                 },
-                'B' | 'b' => MorseLetter {
-                    morse: vec![Dash, Dot, Dot, Dot],
+                'C' | 'c' => MorseLetter {
+                    morse: vec![Dash, Dot, Dash, Dot, LetterBreak],
                 },
-                'B' | 'b' => MorseLetter {
-                    morse: vec![Dash, Dot, Dot, Dot],
+                'D' | 'd' => MorseLetter {
+                    morse: vec![Dash, Dot, Dot, LetterBreak],
                 },
                 'E' | 'e' => MorseLetter {
                     morse: vec![Dot, LetterBreak],
                 },
+                'F' | 'f' => MorseLetter {
+                    morse: vec![Dot, Dot, Dash, Dot, LetterBreak],
+                },
+                'G' | 'g' => MorseLetter {
+                    morse: vec![Dash, Dash, Dot, LetterBreak],
+                },
                 'H' | 'h' => MorseLetter {
-                    morse: vec![Dot, Dot, Dot, Dot],
+                    morse: vec![Dot, Dot, Dot, Dot, LetterBreak],
+                },
+                'I' | 'i' => MorseLetter {
+                    morse: vec![Dot, Dot, LetterBreak],
+                },
+                'J' | 'j' => MorseLetter {
+                    morse: vec![Dot, Dash, Dash, Dash, LetterBreak],
+                },
+                'K' | 'k' => MorseLetter {
+                    morse: vec![Dash, Dot, Dash, LetterBreak],
                 },
                 'L' | 'l' => MorseLetter {
                     morse: vec![Dot, Dash, Dot, Dot, LetterBreak],
                 },
+                'M' | 'm' => MorseLetter {
+                    morse: vec![Dash, Dash, LetterBreak],
+                },
+                'N' | 'n' => MorseLetter {
+                    morse: vec![Dash, Dot, LetterBreak],
+                },
                 'O' | 'o' => MorseLetter {
-                    morse: vec![Dash, Dash, Dash],
+                    morse: vec![Dash, Dash, Dash, LetterBreak],
+                },
+                'P' | 'p' => MorseLetter {
+                    morse: vec![Dot, Dash, Dash, Dot, LetterBreak],
+                },
+                'Q' | 'q' => MorseLetter {
+                    morse: vec![Dash, Dash, Dot, Dash, LetterBreak],
+                },
+                'R' | 'r' => MorseLetter {
+                    morse: vec![Dot, Dash, Dot, LetterBreak],
+                },
+                'S' | 's' => MorseLetter {
+                    morse: vec![Dot, Dot, Dot, LetterBreak],
+                },
+                'T' | 't' => MorseLetter {
+                    morse: vec![Dash, LetterBreak],
+                },
+                'U' | 'u' => MorseLetter {
+                    morse: vec![Dot, Dot, Dash, LetterBreak],
+                },
+                'V' | 'v' => MorseLetter {
+                    morse: vec![Dot, Dot, Dot, Dash, LetterBreak],
+                },
+                'W' | 'w' => MorseLetter {
+                    morse: vec![Dot, Dash, Dash, LetterBreak],
+                },
+                'X' | 'x' => MorseLetter {
+                    morse: vec![Dash, Dot, Dot, Dash, LetterBreak],
+                },
+                'Y' | 'y' => MorseLetter {
+                    morse: vec![Dash, Dot, Dash, Dash, LetterBreak],
+                },
+                'Z' | 'z' => MorseLetter {
+                    morse: vec![Dash, Dash, Dot, Dot, LetterBreak],
                 },
                 ' ' => MorseLetter {
                     morse: vec![WordBreak],
